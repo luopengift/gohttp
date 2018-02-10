c0ba683b6ad0a550ba99ea48808c63522a15c28d
luopengift <870148195@qq.com>
2018-02-10 19:31:30 +0800

    add log to stderr

--- api.go
+++ api.go
@@ -1,11 +1,27 @@
 package gohttp
 
+import (
+	"fmt"
+)
+
 // ApiOutput is sturct data need responsed.
 type ApiOutput struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Err  error       `json:"err"`
 	Data interface{} `json:"data"`
+}
+
+func (api *ApiOutput) Error() string {
+	return fmt.Sprintf("%d: %s", api.Code, api.Msg)
+}
+
+func (api *ApiOutput) String() string {
+	return fmt.Sprintf("%d: %s|%s", api.Code, api.Msg, api.Err.Error())
+}
+
+func (api *ApiOutput) Detail() string {
+	return fmt.Sprintf("%d: %s\n%s\n%v", api.Code, api.Msg, api.Err.Error(), api.Data)
 }
 
 // ApiHandler designed for http api. It can used easy.
@@ -16,4 +32,21 @@ type ApiHandler struct {
 
 func (ctx *ApiHandler) Finish() {
 	ctx.Output(ctx.ApiOutput)
+}
+
+// RegistErrCode regist api error code and msg.
+func RegistErrCode(code int, msg string) error {
+	if value, ok := ErrMap[code]; !ok {
+		return fmt.Errorf("code[%d] is used, msg=%s", code, value)
+	}
+	ErrMap[code] = msg
+	return nil
+}
+
+// ErrMap
+var ErrMap = map[int]string{
+	0:    "success",
+	1000: "success",
+	1001: "fail",
+	1002: "",
 }
--- log.go
+++ log.go
@@ -1,13 +1,14 @@
 package gohttp
 
 import (
+	"os"
 	"github.com/luopengift/log"
 )
 
 // InitLog inits gohttp loghandler
 func InitLog() *log.Log {
 	fileHandler := log.NewFile("/tmp/access_%Y%M%D.log")
-	gohttpLog := log.NewLog("gohttp", fileHandler)
+	gohttpLog := log.NewLog("gohttp", os.Stderr, fileHandler)
 	gohttpLog.SetFormatter(log.NewTextFormat("TIME [LEVEL] MESSAGE", 0))
 	return gohttpLog
 }
